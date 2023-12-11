c618baca033cc6956b22e3aeff31dd6d2950c499
drib <calumholt@hotmail.com>
2023-12-11 10:22:58 -0700

    new

---
+++ src/app/new/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+return (
+  <main>
+    <h1 className="text-xl">MY NEW PAGE</h1>
+    <p className="text-rose-500 m-auto">I created this for the achievement</p>
+  </main>
+)
+}
